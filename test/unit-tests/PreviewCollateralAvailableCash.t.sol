25022ba9280fe503b82e6d14d1a19b23e31334b9
Vitaly Galaichuk <swissarmytowel@gmail.com>
2023-10-04 19:39:19 +0700

    chore: add partial margin tests to preview collat available

--- test/unit-tests/PreviewCollateralAvailableCash.t.sol
+++ test/unit-tests/PreviewCollateralAvailableCash.t.sol
@@ -8,6 +8,7 @@ import "grappa/config/types.sol";
 import "grappa/config/constants.sol";
 
 import "../../src/config/types.sol";
+import "../mocks/MockERC20.sol";
 
 contract TestPreviewCollateralAvailable_CMC is CrossMarginCashFixture {
     function setUp() public {}
@@ -129,6 +130,187 @@ contract TestPreviewCollateralAvailable_CMC is CrossMarginCashFixture {
         assertEq(amounts[0], 0);
         assertEq(amounts[1], 0);
 
+        assertEq(isUnderWater, false);
+    }
+}
+
+contract TestPreviewCollateralAvailablePM_CMC is CrossMarginCashFixture {
+    MockERC20 internal lsEth;
+    MockERC20 internal usyc;
+
+    uint8 internal lsEthId;
+    uint8 internal usycId;
+
+    uint40 internal pidLsEthCollat;
+    uint40 internal pidUsycCollat;
+
+    uint256 public expiry;
+
+    function setUp() public {
+        lsEth = new MockERC20("LsETH", "LsETH", 18);
+        vm.label(address(lsEth), "LsETH");
+
+        usyc = new MockERC20("USYC", "USYC", 6);
+        vm.label(address(usyc), "USYC");
+
+        lsEthId = grappa.registerAsset(address(lsEth));
+        usycId = grappa.registerAsset(address(usyc));
+
+        engine.setCollateralizable(address(weth), address(lsEth), true);
+        engine.setCollateralizable(address(usdc), address(usyc), true);
+
+        pidLsEthCollat = grappa.getProductId(address(oracle), address(engine), address(weth), address(usdc), address(lsEth));
+        pidUsycCollat = grappa.getProductId(address(oracle), address(engine), address(weth), address(usdc), address(usyc));
+
+        lsEth.mint(address(this), 100 * 1e18);
+        usyc.mint(address(this), 1000_000 * 1e6);
+
+        expiry = block.timestamp + 14 days;
+
+        oracle.setSpotPrice(address(lsEth), 3000 * UNIT);
+        oracle.setSpotPrice(address(usyc), 1 * UNIT);
+    }
+
+    function testPreviewCollateralEqualShortLong() public {
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 tokenId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice, 0);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: wethId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(0));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], 0);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralEqualCallSpread() public {
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 strikeSpread = 1 * UNIT;
+
+        uint256 shortId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice - strikeSpread, 0);
+        uint256 longId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice, 0);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: wethId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: shortId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: longId, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        uint256 underlyingRequired = (((strikeSpread * UNIT) / strikePrice) * (10 ** (18 - 6)));
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(weth));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - underlyingRequired);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralEqualPutSpread() public {
+        uint256 depositAmount = 2000 * 1e6;
+
+        uint256 strikePrice = 2000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 strikeSpread = 1;
+
+        uint256 tokenId = getTokenId(TokenType.PUT, pidUsdcCollat, expiry, strikePrice, 0);
+        uint256 tokenId2 = getTokenId(TokenType.PUT, pidUsdcCollat, expiry, strikePrice - strikeSpread, 0);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: usdcId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: tokenId2, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        uint256 strikeSpreadScaled = strikeSpread * (10 ** (6 - 6));
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(usdc));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - strikeSpreadScaled);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralAvailablePut() public {
+        uint256 strikePrice = 2000 * UNIT;
+        uint256 amount = 1 * UNIT;
+        uint256 depositAmount = 2000 * 1e6;
+
+        uint256 tokenId = getTokenId(TokenType.PUT, pidUsycCollat, expiry, strikePrice, 0);
+
+        Position[] memory longs = new Position[](0);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: usycId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        uint256 newUsycPrice = 1_040000; // worth more due to interest ($1.04)
+
+        oracle.setSpotPrice(address(usyc), newUsycPrice);
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(usyc));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - strikePrice * UNIT / newUsycPrice);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralAvailableCall() public {
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 tokenId = getTokenId(TokenType.CALL, pidLsEthCollat, expiry, strikePrice, 0);
+
+        Position[] memory longs = new Position[](0);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: lsEthId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        uint256 newLsEthPrice = 3200 * UNIT;
+        oracle.setSpotPrice(address(lsEth), newLsEthPrice);
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(lsEth));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - depositAmount * UNIT / newLsEthPrice);
         assertEq(isUnderWater, false);
     }
 }
--- test/unit-tests/PreviewCollateralAvailablePhysical.t.sol
+++ test/unit-tests/PreviewCollateralAvailablePhysical.t.sol
@@ -8,6 +8,7 @@ import "pomace/config/types.sol";
 import "pomace/config/constants.sol";
 
 import "../../src/config/types.sol";
+import "../mocks/MockERC20.sol";
 
 contract TestPreviewCollateralAvailable_CMP is CrossMarginPhysicalFixture {
     function setUp() public {}
@@ -130,5 +131,190 @@ contract TestPreviewCollateralAvailable_CMP is CrossMarginPhysicalFixture {
         assertEq(amounts[1], 0);
 
         assertEq(isUnderWater, false);
+    }
+}
+
+contract TestPreviewCollateralAvailablePM_CMP is CrossMarginPhysicalFixture {
+    MockERC20 internal lsEth;
+    MockERC20 internal sdyc;
+
+    uint8 internal lsEthId;
+    uint8 internal sdycId;
+
+    uint32 internal pidLsEthCollat;
+    uint32 internal pidSdycCollat;
+
+    uint256 public expiry;
+
+    function setUp() public {
+        lsEth = new MockERC20("LsETH", "LsETH", 18);
+        vm.label(address(lsEth), "LsETH");
+
+        sdyc = new MockERC20("SDYC", "SDYC", 6);
+        vm.label(address(sdyc), "SDYC");
+
+        lsEthId = pomace.registerAsset(address(lsEth));
+        sdycId = pomace.registerAsset(address(sdyc));
+
+        pomace.setCollateralizable(address(weth), address(lsEth), true);
+        pomace.setCollateralizable(address(usdc), address(sdyc), true);
+
+        pidLsEthCollat = pomace.getProductId(address(engine), address(weth), address(usdc), address(lsEth));
+        pidSdycCollat = pomace.getProductId(address(engine), address(weth), address(usdc), address(sdyc));
+
+        lsEth.mint(address(this), 100 * 1e18);
+        lsEth.approve(address(engine), type(uint256).max);
+
+        sdyc.mint(address(this), 1000_000 * 1e6);
+        sdyc.approve(address(engine), type(uint256).max);
+
+        expiry = block.timestamp + 14 days;
+
+        oracle.setSpotPrice(address(lsEth), 3000 * UNIT);
+        oracle.setSpotPrice(address(sdyc), 1 * UNIT);
+    }
+
+    function testPreviewCollateralEqualShortLong() public {
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 tokenId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice, 30 minutes);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: wethId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(0));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], 0);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralEqualCallSpread() public {
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 strikeSpread = 1 * UNIT;
+
+        uint256 shortId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice - strikeSpread, 30 minutes);
+        uint256 longId = getTokenId(TokenType.CALL, pidEthCollat, expiry, strikePrice, 30 minutes);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: wethId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: shortId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: longId, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        uint256 underlyingRequired = (((strikeSpread * UNIT) / strikePrice) * (10 ** (18 - 6)));
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(weth));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - underlyingRequired);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralEqualPutSpread() public {
+        uint256 depositAmount = 2000 * 1e6;
+
+        uint256 strikePrice = 2000 * UNIT;
+        uint256 amount = 1 * UNIT;
+
+        uint256 strikeSpread = 1;
+
+        uint256 tokenId = getTokenId(TokenType.PUT, pidUsdcCollat, expiry, strikePrice, 30 minutes);
+        uint256 tokenId2 = getTokenId(TokenType.PUT, pidUsdcCollat, expiry, strikePrice - strikeSpread, 30 minutes);
+
+        Position[] memory longs = new Position[](1);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: usdcId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+        longs[0] = Position({tokenId: tokenId2, amount: uint64(amount)});
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        uint256 strikeSpreadScaled = strikeSpread * (10 ** (6 - 6));
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(usdc));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - strikeSpreadScaled);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralAvailablePut() public {
+        uint256 strikePrice = 2000 * UNIT;
+        uint256 amount = 1 * UNIT;
+        uint256 depositAmount = 2000 * 1e6;
+
+        uint256 tokenId = getTokenId(TokenType.PUT, pidSdycCollat, expiry, strikePrice, 30 minutes);
+
+        Position[] memory longs = new Position[](0);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: sdycId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        // uint256 wethExpiryPrice = 1000 * UNIT;
+        uint256 newSdycPrice = 1_040000; // worth more due to interest ($1.04)
+
+        // oracle.setSpotPrice(address(weth), wethExpiryPrice);
+        oracle.setSpotPrice(address(sdyc), newSdycPrice);
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(sdyc));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - strikePrice * UNIT / newSdycPrice);
+        assertEq(isUnderWater, false);
+    }
+
+    function testPreviewCollateralAvailableCall() public {
+        uint256 strikePrice = 4000 * UNIT;
+        uint256 amount = 1 * UNIT;
+        uint256 depositAmount = 1 * 1e18;
+
+        uint256 tokenId = getTokenId(TokenType.CALL, pidLsEthCollat, expiry, strikePrice, 30 minutes);
+
+        Position[] memory longs = new Position[](0);
+        Position[] memory shorts = new Position[](1);
+        Balance[] memory collaterals = new Balance[](1);
+
+        collaterals[0] = Balance({collateralId: lsEthId, amount: uint80(depositAmount)});
+        shorts[0] = Position({tokenId: tokenId, amount: uint64(amount)});
+
+        uint256 newLsEthPrice = 3200 * UNIT;
+        oracle.setSpotPrice(address(lsEth), newLsEthPrice);
+
+        (address[] memory addresses, uint256[] memory amounts, bool isUnderWater) =
+            engine.previewCollateralAvailable(shorts, longs, collaterals);
+
+        assertEq(addresses.length, 1);
+        assertEq(addresses[0], address(lsEth));
+        assertEq(amounts.length, 1);
+        assertEq(amounts[0], depositAmount - depositAmount * UNIT / newLsEthPrice);
     }
 }
