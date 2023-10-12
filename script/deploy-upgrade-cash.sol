c597bb9ec1734edf09be1e9465286ddea5a055e5
dsshap <dss.shapiro@gmail.com>
2023-10-12 11:52:25 -0500

    adding deploy cash and core scripts

---
+++ script/deploy-upgrade-cash.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+
+import "../src/settled-cash/CrossMarginCashEngine.sol";
+
+contract Deploy is Script {
+    function run() external {
+        console.log("Deployer", msg.sender);
+
+        vm.startBroadcast();
+
+        address grappa = vm.envAddress("GrappaProxy");
+        address optionToken = vm.envAddress("GrappaOptionToken");
+
+        // // deploy and register Cross Margin Engine
+        deployCrossMarginEngine(grappa, optionToken);
+
+        vm.stopBroadcast();
+    }
+
+    function deployCrossMarginEngine(address grappa, address optionToken) public {
+        // ============ Deploy Cross Margin Engine (Upgradable) ============== //
+        address engineImplementation =
+            address(new CrossMarginCashEngine(address(grappa), optionToken, vm.envAddress("CrossMarginCashOracle")));
+
+        console.log("CrossMargin Cash Engine: \t\t\t", engineImplementation);
+    }
+
+    // add a function prefixed with test here so forge coverage will ignore this file
+    function testChill() public {}
+}
---
+++ script/deploy-upgrade-physical.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+
+import "../src/settled-physical/CrossMarginPhysicalEngine.sol";
+
+
+contract DeployPhysicalMarginEngine is Script {
+    function run() external {
+        console.log("Deployer", msg.sender);
+
+        vm.startBroadcast();
+
+        // ============ Deploy Cross Margin Engine (Upgradable) ============== //
+        address engineImplementation = address(new CrossMarginPhysicalEngine(vm.envAddress("PomaceProxy"), vm.envAddress("PomaceOptionToken")));
+
+        console.log("CrossMargin Physical Engine: \t\t", engineImplementation);
+
+        vm.stopBroadcast();
+    }
+
+    function deployCrossMarginPhysicalEngine(address pomace, address optionToken) public {
+
+    }
+}
