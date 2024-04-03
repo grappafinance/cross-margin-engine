afe68c859b591301fa1499b72c7df960b94b6733
dsshap <dss.shapiro@gmail.com>
2024-04-03 12:15:17 -0500

    updating logixc slightly

--- src/settled-cash/CrossMarginCashEngine.sol
+++ src/settled-cash/CrossMarginCashEngine.sol
@@ -566,7 +566,10 @@ contract CrossMarginCashEngine is
     function _assertCallerHasAccess(address _subAccount) internal override {
         if (_isPrimaryAccountFor(msg.sender, _subAccount)) return;
 
-        if (authority.doesUserHaveRole(tx.origin, Role.System_FundAdmin) && authority.getUserRoles(msg.sender) != bytes32(0)) {
+        if (
+            authority.doesUserHaveRole(tx.origin, Role.System_FundAdmin)
+                && (msg.sender == tx.origin || authority.getUserRoles(msg.sender) != bytes32(0))
+        ) {
             return;
         }
 
